1a8ac171a68508db5bd339fc12c927349cf3b849
ckarying <ckarying@gmail.com>
2018-09-05 15:41:16 +0800

    format code style

--- rand_policy.py
+++ rand_policy.py
@@ -1,37 +1,42 @@
 #!/usr/bin/env python
-# -*- coding: gbk -*-
-"""
-author: chenkangrui
-email: chenkangrui@baidu.com
-time: 2018-04-15
-"""
+
 import gym
 import time
 import random
-envname = "Ant-v2"
-max_steps = 100
-max_rollouts = 20
-show_render = True
-def main():
+import configparser
+
+config = configparser.ConfigParser()
+config["default"] = {
+    "env_name" : "Alien-v0" ,
+    "max_steps" : 100 ,
+    "max_rollouts" : 20 ,
+    "show_render" : True
+}
+
+def rand_policy(config):
+    envname = config['env_name']
+    max_rollouts = int(config['max_rollouts'])
+    max_steps = int(config['max_steps'])
+    show_render = config['show_render']
+    
     env = gym.make(envname)
     env.seed(0)
-    print(env.action_space)
     for rollout in range(max_rollouts):
         obs = env.reset()
         done = False
         for step in range(max_steps):
-#            action = [random.uniform(0, 1), random.uniform(0, 1)]
-#            print("action:", action)
             action = env.action_space.sample()
             obs, r, done, _ = env.step(action)
-#            print("done:", done)
             if show_render:
                 env.render()
             if done:
                 break
         print("rollout:%d, step:%d, done:%d" %(rollout, step, done))
         time.sleep(5)
-        
+    
+def main():
+    rand_policy(config['default'])
+    
 if __name__ == "__main__":
     main()
 
